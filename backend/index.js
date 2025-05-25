20bb2fa81257356679366cdf3f02792c614ad76f
slobodan1987 <slobodan.opsenica@gmail.com>
2025-05-25 15:30:49 +0200

    Port fix

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,8 @@ app.post("/api/bookings", async (req, res) => {
   res.json({ id: result.lastID, name, date });
 });
 
-app.listen(3000, () => {
-  console.log("Backend running on http://localhost:3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
