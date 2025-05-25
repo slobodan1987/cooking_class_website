8921209c0b7239be029189ce7ab5f185fd360736
slobodan1987 <slobodan.opsenica@gmail.com>
2025-05-25 16:55:28 +0200

    flags src fix

--- frontend/src/app/shared/language-switcher/languages.ts
+++ frontend/src/app/shared/language-switcher/languages.ts
@@ -5,10 +5,10 @@ export interface Language {
 }
 
 export const LANGUAGES: Language[] = [
-  { code: 'en', name: 'ENGLISH', flag: '/cooking_class_website/assets/img/flags/flag-en.svg' },
-  { code: 'hr', name: 'HRVATSKI', flag: '/cooking_class_website/assets/img/flags/flag-hr.svg' },
-  { code: 'de', name: 'DEUTSCH', flag: '/cooking_class_website/assets/img/flags/flag-de.svg' },
-  { code: 'cz', name: 'ČEŠTINA', flag: '/cooking_class_website/assets/img/flags/flag-cz.svg' },
-  { code: 'es', name: 'ESPAÑOL', flag: '/cooking_class_website/assets/img/flags/flag-es.svg' },
-  { code: 'it', name: 'ITALIANO', flag: '/cooking_class_website/assets/img/flags/flag-it.svg' }
+  { code: 'en', name: 'ENGLISH', flag: '/assets/img/flags/flag-en.svg' },
+  { code: 'hr', name: 'HRVATSKI', flag: '/assets/img/flags/flag-hr.svg' },
+  { code: 'de', name: 'DEUTSCH', flag: '/assets/img/flags/flag-de.svg' },
+  { code: 'cz', name: 'ČEŠTINA', flag: '/assets/img/flags/flag-cz.svg' },
+  { code: 'es', name: 'ESPAÑOL', flag: '/assets/img/flags/flag-es.svg' },
+  { code: 'it', name: 'ITALIANO', flag: '/assets/img/flags/flag-it.svg' },
 ];
