0db11a1b489292668005fac5854c1fd86a7b283e
slobodan1987 <slobodan.opsenica@gmail.com>
2025-06-07 08:49:54 +0200

    Prevodi - korak 1

--- frontend/src/app/shared/language-switcher/languages.ts
+++ frontend/src/app/shared/language-switcher/languages.ts
@@ -5,10 +5,11 @@ export interface Language {
 }
 
 export const LANGUAGES: Language[] = [
-  { code: 'en', name: 'ENGLISH', flag: '/assets/img/flags/flag-en.svg' },
   { code: 'hr', name: 'HRVATSKI', flag: '/assets/img/flags/flag-hr.svg' },
+  { code: 'en', name: 'ENGLISH', flag: '/assets/img/flags/flag-en.svg' },
   { code: 'de', name: 'DEUTSCH', flag: '/assets/img/flags/flag-de.svg' },
-  { code: 'cz', name: 'ČEŠTINA', flag: '/assets/img/flags/flag-cz.svg' },
-  { code: 'es', name: 'ESPAÑOL', flag: '/assets/img/flags/flag-es.svg' },
   { code: 'it', name: 'ITALIANO', flag: '/assets/img/flags/flag-it.svg' },
+  { code: 'fr', name: 'FRANÇAIS', flag: '/assets/img/flags/flag-fr.svg' },
+  { code: 'es', name: 'ESPAÑOL', flag: '/assets/img/flags/flag-es.svg' },
+  { code: 'cz', name: 'ČEŠTINA', flag: '/assets/img/flags/flag-cz.svg' },
 ];
