f0e91ab25f6eb1b250dd3582f4bd8cec71c7b42a
slobodan1987 <slobodan.opsenica@gmail.com>
2025-06-08 17:34:40 +0200

    hhhhjjj

--- frontend/merge-locales.sh
+++ frontend/merge-locales.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-# Podesi koji je glavni jezik (ide na root /)
-DEFAULT_LANG="hr"
-
 # Ostali jezici koji idu u poddirektorijume
-OTHER_LANGS=("en-US" "de" "fr" "it" "es" "cs")
+LANGS=("en-US" "de" "fr" "it" "es" "cs" "hr")
 
 # Očisti i pripremi dist/cooking_class_website/merged
 rm -rf dist/cooking_class_website/merged
 mkdir -p dist/cooking_class_website/merged
-echo "✅ dist/cooking_class_website/merged rekreiran"
-
-# HR u root (kao default)
-cp -r dist/cooking_class_website/browser/$DEFAULT_LANG/* dist/cooking_class_website/merged/
-echo "✅ HR prebacen u dist/cooking_class_website/merged"
 
 # Ostali jezici u podfoldere
-for lang in "${OTHER_LANGS[@]}"; do
-cp dist/cooking_class_website/browser/$lang/index.html dist/cooking_class_website/merged/${lang}.html
+for lang in "${LANGS[@]}"; do
+  sed -i "s/main\.js/main-${lang}.js/g" dist/cooking_class_website/browser/$lang/index.html
+  mv dist/cooking_class_website/browser/$lang/index.html dist/cooking_class_website/browser/$lang/index-$lang.html
+  mv dist/cooking_class_website/browser/$lang/main.js dist/cooking_class_website/browser/$lang/main-$lang.js
+  cp -r dist/cooking_class_website/browser/$lang/* dist/cooking_class_website/merged/
 done
-echo "✅ svi jezici prebaceni u dist/cooking_class_website/merged"
-
 # Očisti originalne foldere
 rm -rf dist/cooking_class_website/browser
-echo "✅ originalni folderi obrisani"
 
 echo "✅ dist/cooking_class_website/merged spreman za deploy:"
-echo " - /       → $DEFAULT_LANG"
-for lang in "${OTHER_LANGS[@]}"; do
-  echo " - /$lang/ → $lang"
-done
